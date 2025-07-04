771759fa2362054d736c694ae7f24dbae707a15b
N-Yash <113545575+N-Yash@users.noreply.github.com>
2025-07-04 17:52:32 -0400

    Week 7 incomplete

---
+++ Week7/Week7/Week7App.swift
@@ -0,0 +1,30 @@
+//
+//  Week7App.swift
+//  Week7
+//
+//  Created by Yash Vipul Naik on 2025-07-04.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct Week7App: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
